8a2ba695c0f84487320e69378130df14b031fe26
Michael Barkov <geek@bk.ru>
2019-11-24 21:12:19 +0300

    fix search

--- app/controllers/SmSearch.scala
+++ app/controllers/SmSearch.scala
@@ -98,7 +98,7 @@ class SmSearch @Inject()(val database: DBService)(implicit assetsFinder: AssetsF
         fc.storeName === device.uid
       })
     } yield (fcRow, device))
-      .groupBy(uRow => (uRow._1.fName, uRow._1.fParent, uRow._1.sha256, uRow._2.map(_.label)))
+      .groupBy(uRow => (uRow._1.fNameLc, uRow._1.fName, uRow._1.fParent, uRow._1.sha256, uRow._2.map(_.label)))
       .map({ case (uRow, cnt) => (uRow, cnt.map(_._1).length) })
 
     val cntAll = baseQry.length.result
@@ -107,14 +107,14 @@ class SmSearch @Inject()(val database: DBService)(implicit assetsFinder: AssetsF
     val qryBySearch = filtered.drop(start).take(length)
 
     val sortedQ = (sortCol, sortDir) match {
-      case (0, "desc") => qryBySearch.sortBy(_._1._1.desc)
-      case (1, "asc") => qryBySearch.sortBy(_._1._2)
-      case (1, "desc") => qryBySearch.sortBy(_._1._2.desc)
-      case (2, "asc") => qryBySearch.sortBy(_._1._3)
-      case (2, "desc") => qryBySearch.sortBy(_._1._3.desc)
-      case (3, "asc") => qryBySearch.sortBy(_._1._4)
-      case (3, "desc") => qryBySearch.sortBy(_._1._4.desc)
-      case (_, _) => qryBySearch.sortBy(_._1._1)
+      case (0, "desc") => qryBySearch.sortBy(_._1._2.desc)
+      case (1, "asc") => qryBySearch.sortBy(_._1._3)
+      case (1, "desc") => qryBySearch.sortBy(_._1._3.desc)
+      case (2, "asc") => qryBySearch.sortBy(_._1._4)
+      case (2, "desc") => qryBySearch.sortBy(_._1._4.desc)
+      case (3, "asc") => qryBySearch.sortBy(_._1._5)
+      case (3, "desc") => qryBySearch.sortBy(_._1._5.desc)
+      case (_, _) => qryBySearch.sortBy(_._1._2)
     }
 
     val composedAction = for {cntAll <- cntAll
@@ -124,7 +124,7 @@ class SmSearch @Inject()(val database: DBService)(implicit assetsFinder: AssetsF
 
     database.runAsync(composedAction).map { rowSeq =>
       val filePath = ArrayBuffer[FilePath]()
-      rowSeq._3.foreach { p => filePath += FilePath(name = p._1._1, path = p._1._2, sha256 = p._1._3.getOrElse(""), device = p._1._4.getOrElse("")) }
+      rowSeq._3.foreach { p => filePath += FilePath(name = p._1._2, path = p._1._3, sha256 = p._1._4.getOrElse(""), device = p._1._5.getOrElse("")) }
 
       val ret = Paging(draw, rowSeq._1, rowSeq._2, filePath.toSeq, "")
 
