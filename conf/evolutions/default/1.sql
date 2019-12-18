95f320dd2b71093cf32d406de1069a37fb7dad19
Michael Barkov <m.barkov@binbank.ru>
2019-12-18 18:08:55 +0300

    save

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,18 +1,18 @@
 # --- !Ups
 
-CREATE TABLE "public".sm_category_fc
+CREATE TABLE sm_category_fc
 (
   id                varchar NOT NULL,
   f_name            varchar NOT NULL,
   category_type     varchar,
   sub_category_type varchar,
   description       varchar,
---   CONSTRAINT sm_category_fc_pkey PRIMARY KEY (id, f_name)
+  CONSTRAINT sm_category_fc_pkey PRIMARY KEY (id, f_name)
 );
 
-CREATE TABLE "public".sm_device
+CREATE TABLE sm_device
 (
-  id          serial PRIMARY KEY,
+  id          serial            NOT NULL,
   name        varchar           NOT NULL,
   label       varchar           NOT NULL,
   device_uid  varchar           NOT NULL,
@@ -20,10 +20,11 @@ CREATE TABLE "public".sm_device
   description varchar,
   visible     bool DEFAULT true NOT NULL,
   reliable    bool DEFAULT true NOT NULL,
-  --   CONSTRAINT sm_device_pkey PRIMARY KEY (id)
+  CONSTRAINT sm_device_pkey PRIMARY KEY (id),
+  CONSTRAINT idx_sm_device_device_uid UNIQUE (device_uid)
 );
 
-CREATE TABLE "public".sm_exif
+CREATE TABLE sm_exif
 (
   id                    varchar NOT NULL,
   date_time             timestamp,
@@ -44,52 +45,54 @@ CREATE TABLE "public".sm_exif
   gps_time_stamp        varchar,
   gps_processing_method varchar,
   gps_date_stamp        varchar,
-  gps_latitude_dec      numeric(-9999999,0),
-  gps_longitude_dec     numeric(-9999999,0),
-  --   CONSTRAINT sm_exif_pkey PRIMARY KEY (id)
+  gps_latitude_dec      decimal,
+  gps_longitude_dec     decimal,
+  CONSTRAINT sm_exif_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE "public".sm_file_card
+CREATE TABLE sm_file_card
 (
-  id                   VARCHAR PRIMARY KEY NOT NULL,
-  device_uid           varchar             NOT NULL,
-  f_parent             varchar             NOT NULL,
-  f_name               varchar             NOT NULL,
+  id                   varchar   NOT NULL,
+  device_uid           varchar   NOT NULL,
+  f_parent             varchar   NOT NULL,
+  f_name               varchar   NOT NULL,
   f_extension          varchar,
-  f_creation_date      timestamp           NOT NULL,
-  f_last_modified_date timestamp           NOT NULL,
+  f_creation_date      timestamp NOT NULL,
+  f_last_modified_date timestamp NOT NULL,
   f_size               bigint,
   f_mime_type_java     varchar,
   sha256               varchar,
-  f_name_lc            varchar             NOT NULL,
-  --   CONSTRAINT sm_file_card_pkey PRIMARY KEY (id)
+  f_name_lc            varchar   NOT NULL,
+  CONSTRAINT sm_file_card_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_sm_file_card_sm_device FOREIGN KEY (device_uid) REFERENCES sm_device (device_uid)
 );
 
-CREATE INDEX f_parent_idx ON "public".sm_file_card ( f_parent ASC NULLS LAST);
+CREATE INDEX idx_f_parent ON sm_file_card ( f_parent ASC NULLS LAST);
 
-CREATE INDEX last_modified_idx ON "public".sm_file_card ( f_last_modified_date DESC NULLS LAST);
+CREATE INDEX idx_fc_f_name_lc ON sm_file_card(f_name_lc ASC NULLS LAST);
 
-CREATE INDEX sha256_idx ON "public".sm_file_card ( sha256 ASC NULLS LAST);
+CREATE INDEX idx_last_modified ON sm_file_card ( f_last_modified_date DESC NULLS LAST);
 
+CREATE INDEX idx_sha256 ON sm_file_card ( sha256 ASC NULLS LAST);
 
-CREATE TABLE "public".sm_job_path_move
+CREATE TABLE sm_job_path_move
 (
-  id         serial PRIMARY KEY,
+  id         serial  NOT NULL,
   device_uid varchar NOT NULL,
   path_from  varchar NOT NULL,
   path_to    varchar NOT NULL,
   done       bool DEFAULT false,
---   CONSTRAINT sm_path_move_pkey PRIMARY KEY (id),
+  CONSTRAINT sm_job_path_move_pkey PRIMARY KEY (id)
 );
 
 # --- !Downs
 
-DROP TABLE IF EXISTS sm_device CASCADE;
-
 DROP TABLE IF EXISTS sm_file_card CASCADE;
 
-DROP TABLE IF EXISTS sm_path_move CASCADE;
+DROP TABLE IF EXISTS sm_job_path_move CASCADE;
 
 DROP TABLE IF EXISTS sm_category_fc CASCADE;
 
 DROP TABLE IF EXISTS sm_exif CASCADE;
+
+DROP TABLE IF EXISTS sm_device CASCADE;
